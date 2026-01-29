2dc87d6fcee9fcd0f6e5c1aa53ae2b839e7f8e5e
Marcus Negron <manegron1111@gmail.com>
2026-01-28 19:16:37 -0500

    Update testing1.java

--- testing1.java
+++ testing1.java
@@ -2,5 +2,7 @@ public class testing1 {
     public static void main(String[] args) {
         System.out.println("Hello from testing1.java!");
         System.out.println("6767676767676767676767676767");
+        int a = 37;
+        System.out.println(a);
     }
 }
